7916fb18c9602189504fc3a002cec0d226746bba
이혜진 <annie_1229@naver.com>
2022-07-20 17:49:35 +0900

    fix: mypage

--- components/mypage/gamePlayer.js
+++ components/mypage/gamePlayer.js
@@ -1,14 +1,14 @@
 import Image from 'next/image';
 import styles from '../../styles/pages/mypage.module.scss';
 
-export default function GamePlayer({ info, myInfo=null, onClickPlayer }) {
+export default function GamePlayer({ info, onClickPlayer, myInfo=null }) {
   return (
     <div className={styles.gameHistoryPlayerItem}>
       <div className={styles.gameHistoryPlayerProfileImage}>
-        <Image src={myInfo ?? myInfo.avatarUrl} width={20} height={20} alt="profile" className={styles.gameHistoryPlayerProfileImage} />
+        <Image src={myInfo ? myInfo.avatarUrl : info.avatarUrl} width={20} height={20} alt="profile" className={styles.gameHistoryPlayerProfileImage} />
       </div>
       <div className={styles.gameHistoryPlayerInfoBox}>
-        <div className={styles.gameHistoryPlayerNickname}>{myInfo ?? myInfo.gitId}</div>
+        <div className={styles.gameHistoryPlayerNickname}>{myInfo ? myInfo.gitId : info.gitId}</div>
         <div className={styles.gameHistoryPlayerPassRate}>{`✅ ${info.passRate}%`}</div>
         <div className={styles.gameHistoryPlayerLanguage} onClick={onClickPlayer}>{info.language}</div>
       </div>
