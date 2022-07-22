58d55d71eb0467105671839eec1ba54660edb1da
이혜진 <annie_1229@naver.com>
2022-07-22 22:32:42 +0900

    feat: mypage player rank

--- components/mypage/gamePlayer.js
+++ components/mypage/gamePlayer.js
@@ -1,9 +1,38 @@
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from '../../styles/pages/mypage.module.scss';
 
 export default function GamePlayer({ info, onClickPlayer, myInfo=null }) {
+  const [rankText, setRankText] = useState(info.ranking);
+  const [isEmoji, setIsEmoji] = useState(false);
+
+  useEffect(() => {
+    switch(info.ranking) {
+      case 0:
+        setRankText('-');
+        setIsEmoji(false);
+        break;
+      case 1:
+        setRankText('🥇');
+        setIsEmoji(true);
+        break;
+      case 2:
+        setRankText('🥈');
+        setIsEmoji(true);
+        break;
+      case 3:
+        setRankText('🥉');
+        setIsEmoji(true);
+        break;
+      default:
+        setRankText(info.ranking);
+        break;
+    }
+  }, [info.ranking]);
+
   return (
     <div className={styles.gameHistoryPlayerItem}>
+      <div className={isEmoji ? styles.rankEmoji : styles.gameHistoryPlayerRanking}>{rankText}</div>
       <div className={styles.gameHistoryPlayerProfileImage}>
         <Image src={myInfo ? myInfo.avatarUrl : info.avatarUrl} width={20} height={20} alt="profile" className={styles.gameHistoryPlayerProfileImage} />
       </div>
