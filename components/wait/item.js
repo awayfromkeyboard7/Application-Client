05af9a2f3bc97d63d5df176cbd09e868782559f5
이혜진 <annie_1229@naver.com>
2022-07-30 17:41:27 +0900

    fix: wait화면 isPlayer 아니면 userPopup 안뜨게

--- components/wait/item.js
+++ components/wait/item.js
@@ -3,7 +3,7 @@ import styles from '../../styles/components/wait.module.scss';
 
 export default function WaitItem({ info, onClickId }) {
   return (
-    <div className={styles.waitItem} onClick = {onClickId}>
+    <div className={styles.waitItem} onClick={info.isPlayer ? onClickId : () => {}}>
       <div className={styles.profileIcon}>
         <Image src={info.avatarUrl ?? '/default_profile.jpg'} layout="fill" objectFit="cover" />
       </div>
